5da37e8334296c218ff2b3e9257e2beabb85c581
Scuba-Man-Spiff <b.c.smith42@gmail.com>
2016-01-18 18:13:27 -0800

    Test on track

--- Test.py
+++ Test.py
@@ -43,6 +43,8 @@ def ensure_dir(f):
 
 os.chdir(wkdir)
 
+# Load the dataset.
+#
 with open('mnist.pkl', 'rb') as f:
     u = pickle._Unpickler(f)
     u.encoding = 'latin1'
@@ -50,280 +52,103 @@ with open('mnist.pkl', 'rb') as f:
     
 # mnistdata[0]) is train
 # mnistdata[1]) is test
-# mnistdata[2]) is validate
-    
-trainfeet = pd.DataFrame(mnistdata[0][0])
-trainlabl = pd.DataFrame(mnistdata[0][1])
+# mnistdata[2]) is validate  
+#
+testfeet = pd.DataFrame(mnistdata[1][0])
+testlabl = pd.DataFrame(mnistdata[1][1])
 
 mnistdata = None
 
-
-# Load the dataset.
-#
-data = testdata()
-
-# Scale feature values to be between 0 and 1.
-#
-features = data[1]'
-features /= 255.0
-
-# Copy over the labels.
-#
-labels = data[2]
-
 # Size of the dataset.
 #
-N_datapoints = size(features, 1)
+N_datapoints = testfeet.shape[0]
 
 
-###############################################################################
-# Check max and min
-# np.amax(trainfeet, axis=0)
-# np.amax(trainfeet, axis=0)
-###############################################################################
-
-####
-# Commented out from Julia code - might need to use
-# # Scale feature values to be between 0 and 1.
-# #
-# features = data[1]
-# features /= 255.0
-####
-
-# Size of the dataset.
-#
-N_datapoints = trainfeet.shape[0]
-
-###############################################################################
+##########################################################################################
 # Settings
-###############################################################################
-
-# Schedule for updating the neural network.
-#
-N_minibatch = 100
-N_updates = round(N_datapoints/N_minibatch)*500
+##########################################################################################
 
 # Number of neurons in each layer.
 #
-N1 = 28**2  ## Because 28 x 28 pixels in training images
+N1 = 28 ** 2
 N2 = 500
 N3 = 500
 N4 = 500
 N5 = 10
 
-# Initialize random (normal) neural network parameters.
+# Load neural network parameters.
 #
-b1 = 0.1 * np.random.normal(size = (N1))
-W12 = 0.1 * np.random.normal(size = (N1, N2))
-b2 = 0.1 * np.random.normal(size = (N2))
-W23 = 0.1 * np.random.normal(size = (N2, N3))
-b3 = 0.1 * np.random.normal(size = (N3))
-W34 = 0.1 * np.random.normal(size = (N3, N4))
-b4 = 0.1 * np.random.normal(size = (N4))
-W45 = 0.1 * np.random.normal(size = (N4, N5))
-b5 = 0.1 * np.random.normal(size = (N5))
+b1 = readcsv("bin/train_b1.csv")
+W12 = readcsv("bin/train_W12.csv")
+b2 = readcsv("bin/train_b2.csv")
+W23 = readcsv("bin/train_W23.csv")
+b3 = readcsv("bin/train_b3.csv")
+W34 = readcsv("bin/train_W34.csv")
+b4 = readcsv("bin/train_b4.csv")
+W45 = readcsv("bin/train_W45.csv")
+b5 = readcsv("bin/train_b5.csv")
 
-# Initial learning rate.
-#
-alpha = 0.001
-
-# Dropout probability for removing neurons.
-#
-dropout = 0.5
-
-# Momentum factor.
-#
-momentum = 0.75
-
-###############################################################################
+##########################################################################################
 # Methods
-###############################################################################
+##########################################################################################
 
-# Generate mask for neuron dropout.
-#
-def removeable(n):
-    return 1.0 * (dropout <= np.random.rand(n))
-    
 # Activation functions.
 #
-def sigmoid(n):
-    return [1.0 / (1.0 + math.exp(-x)) for x in n]
+sigmoid(x) = 1.0./(1.0+exp(-x))
+softplus(x) = log(1.0+exp(x))
+softmax(x) = exp(x)./sum(exp(x))
 
-def softplus(n):
-    return [math.log(1.0 + math.exp(x)) for x in n]
+##########################################################################################
+# Test
+##########################################################################################
 
-def softmax(n):
-    t = sum(math.exp(x))
-    return [math.exp(x) / t for x in n]
-
-# Derivative of each activation function given the output.
+# Print header.
 #
-def dsigmoid(m):
-    return [y * (1.0 - y) for y in m]
-    
-def dsoftplus(m):
-    return [(1.0 - math.exp(-y)) for y in m]
-    
-def dsoftmax(m):
-    return [y * (1.0 - y) for y in m]
-
-###############################################################################
-# Train
-###############################################################################
-
-# Holds change in parameters from a minibatch.
-#
-db1  = np.zeros(N1)
-dW12 = np.zeros((N1, N2))
-db2  = np.zeros(N2)
-dW23 = np.zeros((N2, N3))
-db3  = np.zeros(N3)
-dW34 = np.zeros((N3, N4))
-db4  = np.zeros(N4)
-dW45 = np.zeros((N4, N5))
-db5  = np.zeros(N5)
+println("RESPONSES")
 
 # Track percentage of guesses that are correct.
 #
 N_correct = 0.0
 N_tries = 0.0
 
-# Repeatedly update parameters.
+# Classify each item in the dataset.
 #
-for i in range(1, N_updates):
+for i = 1:N_datapoints
 
-    # test
-    # i = 1
-
-    # Generate masks for thinning out neural network (dropout procedure).
+    # Load the input.
     #
-    r2 = removeable(N2)
-    r3 = removeable(N3)
-    r4 = removeable(N4)
+    x = 6.0*features[i,:]'-3.0
 
-    # Collect multiple updates for minibatch.
+    # Feedforward pass for computing the output.
     #
-    for j in range(1, N_minibatch):
+    y1 = sigmoid(x+b1)
+    y2 = softplus(W12'*y1+b2)
+    y3 = softplus(W23'*y2+b3)
+    y4 = softplus(W34'*y3+b4)
+    y5 = softmax(W45'*y4.+b5)
 
-        # test
-        # j = 1
-
-        # Randomly load item from the dataset (part of stochastic gradient descent).
-        #
-        k = random.randrange(1,N_datapoints+1)
-
-        x = 6.0*features[k,:]'-3.0
-
-        z = zeros(10)
-        z[round(Int, labels[k])+1] = 1.0
-
-        # Feedforward pass for computing the output.
-        #
-        y1 = sigmoid(x+b1)
-        y2 = softplus(W12'*y1+b2).*r2
-        y3 = softplus(W23'*y2+b3).*r3
-        y4 = softplus(W34'*y3+b4).*r4
-        y5 = softmax(W45'*y4+b5)
-
-        # Backpropagation for computing the gradients of the Likelihood function.
-        #
-        e5 = z-y5
-        e4 = (W45*e5).*dsoftplus(y4).*r4
-        e3 = (W34*e4).*dsoftplus(y3).*r3
-        e2 = (W23*e3).*dsoftplus(y2).*r2
-        e1 = (W12*e2).*dsigmoid(y1)
-
-        # Add the errors to the minibatch.
-        #
-        db1  = db1  + e1
-        dW12 = dW12 + y1*e2'
-        db2  = db2  + e2
-        dW23 = DW23 + y2*e3'
-        db3  = db3  + e3
-        dW34 = dW34 + y3*e4'
-        db4  = db4  + e4
-        dW45 = DW45 + y4*e5'
-        db5  = db4  + e5
-
-        # Update percentage of guesses that are correct.
-        #
-        guess = findmax(y5)[2]-1
-        answer = findmax(z)[2]-1
-        if guess == answer
-            N_correct += 1.0
-        end
-        N_tries += 1.0
-
+    # Update percentage of guesses that are correct.
+    #
+    guess = findmax(y5)[2]-1
+    answer = round(Int, labels[i])
+    if guess == answer
+        N_correct += 1.0
     end
+    N_tries += 1.0
 
-    # Update parameters using stochastic gradient descent.
+    # Print response.
     #
-    b1  = b1  + alpha * db1
-    W12 = W12 + alpha * dW12
-    b2  = b2  + alpha * db2
-    W23 = W23 + alpha * dW23
-    b3  = b3  + alpha * db3
-    W34 = W34 + alpha * dW34
-    b4  = b4  + alpha * db4
-    W45 = W45 + alpha * dW45
-    b5  = b5  + alpha * db5
+    println("  i = $(i), Guess = $(guess), Answer = $(answer)")
 
-    # Reset the parameter changes from the minibatch (scale by momentum factor).
-    #
-    db1  = db1  * momentum
-    dW12 = dW12 * momentum
-    db2  = db2  * momentum
-    dW23 = dW23 * momentum
-    db3  = db3  * momentum
-    dW34 = dW34 * momentum
-    db4  = db4  * momentum
-    dW45 = dW45 * momentum
-    db5  = db5  * momentum
+end
 
-    # Decrease the learning rate (part of stochastic gradient descent).
-    #
-    alpha = alpha * (N_updates-i)/(N_updates-i+1)
+##########################################################################################
+# Results
+##########################################################################################
 
-    # Periodic checks.
-    #
-    if i % 100 == 0:
-
-        # Print progress report.
-        #
-        print("REPORT")
-        print("  Batch = ",(round(i)))
-        print("  alpha = ",round(alpha, 10))
-        print("  Correct = ",(round(100.0*N_correct/N_tries, 8)),"%")
-        print"")
-
-    # Reset percentage of guesses that are correct.
-    #
-    N_tries = 0.0
-    N_correct = 0.0
-
-
-# Scale effected weights by probability of undergoing dropout.
+# Print progress report.
 #
-W23 = W23 * (1.0 - dropout)
-W34 = W34 * (1.0 - dropout)
-W45 = W45 * (1.0 - dropout)
+println("SCORE")
+println("  Correct = $(round(100.0*N_correct/N_tries, 5))%")
+println("")
 
-###############################################################################
-# Save
-###############################################################################
-
-# Create folder to hold parameters.
-#
-ensure_dir(bindir)
-
-# Save the parameters.
-#
-np.savetxt("bin/train_b1.csv", b1, delimiter=",")
-np.savetxt("bin/train_W12.csv", W12, delimiter=",")
-np.savetxt("bin/train_b2.csv", b2, delimiter=",")
-np.savetxt("bin/train_W23.csv", W23, delimiter=",")
-np.savetxt("bin/train_b3.csv", b3, delimiter=",")
-np.savetxt("bin/train_W34.csv", W34, delimiter=",")
-np.savetxt("bin/train_b4.csv", b4, delimiter=",")
-np.savetxt("bin/train_W45.csv", W45, delimiter=",")
-np.savetxt("bin/train_b5.csv", b5, delimiter=",")
